f9751af07512292c11f5b71e720dd8bc273c6402
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2015-06-22 20:48:36 +0700

    Edit option value

--- 2.php
+++ 2.php
@@ -1,17 +1,19 @@
 <?php
 	include "header.php";
-	$P1 = array(g02);
-	$P2 = array(g01,g02);
-	$P3 = array(g01,g03);
-	$P4 = array(g04);
-	$P5 = array(g05);
-	$P6 = array(g06);
-	$P7 = array(g07);
-	$P8 = array(g08, g10);
-	$P9 = array(g08,g09);
-	$P10 = array(g07,g10);
+	$P1 = array("g02");
+	$P2 = array("g01", "g02");
+	$P3 = array("g01", "g03");
+	$P4 = array("g04");
+	$P5 = array("g05");
+	$P6 = array("g06");
+	$P7 = array("g07");
+	$P8 = array("g08", "g10");
+	$P9 = array("g08", "g09");
+	$P10 = array("g07", "g10");
+	$hasil = array();
 	$getdata = mysql_query("SELECT * FROM gejala");
 
+	while()
 ?>
 
 
